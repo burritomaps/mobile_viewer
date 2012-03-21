baa9f177f1963a4c6bea6b973c94f1f0124464d4
Max Ogden <mogden@gmail.com>
2012-03-21 14:50:26 -0700

    convert to node couchapp

---
+++ README.md
@@ -0,0 +1,9 @@
+# Mobile Geo Data Viewer
+
+*Note: This software is a CouchApp*
+
+CouchApps are web applications which can be served directly from [CouchDB](http://couchdb.apache.org). This gives them the nice property of replicating just like any other data stored in CouchDB. They are also simple to write as they can use the built-in jQuery libraries and plugins that ship with CouchDB.
+
+[More info about CouchApps here.](http://couchapp.org)
+
+1. *Get data ready* - There are a lot of ways to do this.  I've written a [case study documenting how we did this for the City of Boston](http://mertonium.com/2011/09/a-data-liberation-walkthrough/).  You should have at least [these fields](https://github.com/mertonium/public_art_finder/blob/master/public_art_finder_template.json) in your documents.
---
+++ couchapp.js
@@ -0,0 +1,207 @@
+var couchapp = require('couchapp')
+var path = require('path')
+
+ddoc = 
+  { "_id":'_design/mobile_viewer',
+    "description": "This mobile website can sit on top of properly formatted public art data and display it on a map.",
+    "name": "Public Art Finder",
+    "rewrites": [
+      {
+        "to": "favicon.ico",
+        "from": "favicon.ico"
+      },
+      {
+        "to": "stylesheets/*",
+        "from": "stylesheets/*"
+      },
+      {
+        "to": "javascripts/*",
+        "from": "javascripts/*"
+      },
+      {
+        "to": "vendor/*",
+        "from": "vendor/*"
+      },
+      {
+        "to": "images/*",
+        "from": "images/*"
+      },
+      {
+        "to": "../../*",
+        "from": "dbimgs/*"
+      },
+      {
+        "to": "_list/jsonp/assetid",
+        "from": "/assetid"
+      },
+      {
+        "to": "index.html",
+        "from": "/"
+      },
+      {
+        "to": "details.html",
+        "from": "details.html"
+      },
+      {
+        "to": "_spatial/_list/geojson/full",
+        "from": "/data"
+      },
+      {
+        "to": "_list/jsonp/config",
+        "from": "/config"
+      },
+      {
+        "to": "./",
+        "from": "ddoc"
+      },
+      {
+        "to": "../../",
+        "from": "api"
+      },
+      {
+        "to": "../../*",
+        "from": "api/*"
+      }
+    ]
+  };
+
+ddoc.views = {
+  assetid: {
+    map: function(doc) {
+      emit(doc._id, doc);
+    }
+  },
+  attach-count: {
+    map: function(doc) {
+      if (doc.source =='San Francisco Arts Commission') {
+        if (doc._attachments) emit(null, Object.keys(Object(doc._attachments)).length);
+      }
+    }
+  },
+  config: {
+    map: function(doc) {
+      if(doc.doc_type && doc.doc_type === "config") {
+        emit(doc._id, doc);
+      }
+    }
+  },
+  recent-items: {
+    map: function(doc) {
+      if (doc.created_at) {
+        emit(doc.created_at, doc);
+      }
+    }
+  },
+  sfonly: {
+    map: function(doc) { 
+      if(doc.source =='San Francisco Arts Commission') emit(doc.accession_id, doc);
+    }
+  }
+}
+
+ddoc.spatial = {
+  /**
+   * A simple spatial view that emits the GeoJSON plus the complete documents.
+   */
+  full: function(doc) {
+    if(doc.geometry) {
+      emit(doc.geometry, doc)
+    }
+  }
+}
+
+ddoc.lists = {
+  asset: function(head, req) {
+    var row, out, sep = '\n';
+
+    // Send the same Content-Type as CouchDB would
+    if (req.headers.Accept.indexOf('application/json')!=-1)
+      start({"headers":{"Content-Type" : "application/json"}});
+    else
+      start({"headers":{"Content-Type" : "text/plain"}});
+
+    if ('callback' in req.query) send(req.query['callback'] + "(");
+
+
+    while (row = getRow()) {
+        out = JSON.stringify(row.value);
+        send(out);
+    }
+    if ('callback' in req.query) send(")");
+  },
+  /**
+   * This function outputs a GeoJSON FeatureCollection (compatible with
+   * OpenLayers). The geometry is stored in the geometry property, all other
+   * properties in the properties property.
+   * 
+   * @author Volker Mische
+   */
+  geojson: function(head, req) {
+    var row, out, sep = '\n';
+
+    // Send the same Content-Type as CouchDB would
+    if (typeof(req.headers.Accept) != "undefined" && req.headers.Accept.indexOf('application/json')!=-1)
+      start({"headers":{"Content-Type" : "application/json"}});
+    else
+      start({"headers":{"Content-Type" : "text/plain"}});
+
+    if ('callback' in req.query) send(req.query['callback'] + "(");
+
+    send('{"type": "FeatureCollection", "features":[');
+    while (row = getRow()) {
+        out = '{"type": "Feature", "id": ' + JSON.stringify(row.id);
+        out += ', "geometry": ' + JSON.stringify(row.value.geometry);
+        delete row.value.geometry;
+        out += ', "properties": ' + JSON.stringify(row.value) + '}';
+
+        send(sep + out);
+        sep = ',\n';
+    }
+    send("]}");
+    if ('callback' in req.query) send(")");
+  },
+  jsonp: function(head, req) {
+    var row, out, sep = '\n';
+
+    // Send the same Content-Type as CouchDB would
+    if (req.headers.Accept.indexOf('application/json')!=-1)
+      start({"headers":{"Content-Type" : "application/json"}});
+    else
+      start({"headers":{"Content-Type" : "text/plain"}});
+
+    if ('callback' in req.query) send(req.query['callback'] + "(");
+
+
+    while (row = getRow()) {
+        out = JSON.stringify(row.value);
+        send(out);
+    }
+    if ('callback' in req.query) send(")");
+  },
+  sum: function(head, req) {
+    var row, out, sum = 0, sep = '\n';
+
+    // Send the same Content-Type as CouchDB would
+    if (req.headers.Accept.indexOf('application/json')!=-1)
+      start({"headers":{"Content-Type" : "application/json"}});
+    else
+      start({"headers":{"Content-Type" : "text/plain"}});
+
+    if ('callback' in req.query) send(req.query['callback'] + "(");
+
+
+    while (row = getRow()) {
+        sum += parseFloat(row.value);
+    }
+
+    out = JSON.stringify(sum);
+    send(out);
+
+    if ('callback' in req.query) send(")");
+  }
+}
+
+
+couchapp.loadAttachments(ddoc, path.join(__dirname, 'attachments'));
+
+module.exports = ddoc;
